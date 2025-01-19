a2689a4c02661b30d07a7dab0e6818a7b6d0396c
Rafaela Vieira Cabral <rafaelavieiracabral04@gmail.com>
2025-01-19 16:48:52 -0300

    feat: conversão dos dados da API

--- src/main/java/br/com/alura/literalura/model/Livro.java
+++ src/main/java/br/com/alura/literalura/model/Livro.java
@@ -1,4 +1,72 @@
 package br.com.alura.literalura.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String idioma;
+
+    @Column(name = "downloads")
+    private Integer numeroDownloads;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    // Construtores, getters e setters
+    public Livro() {}
+
+    public Livro(String titulo, String idioma, Integer numeroDownloads, Autor autor) {
+        this.titulo = titulo;
+        this.idioma = idioma;
+        this.numeroDownloads = numeroDownloads;
+        this.autor = autor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Integer getNumeroDownloads() {
+        return numeroDownloads;
+    }
+
+    public void setNumeroDownloads(Integer numeroDownloads) {
+        this.numeroDownloads = numeroDownloads;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
 }
